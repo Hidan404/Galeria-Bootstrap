824466a9f5ec22eb49f4ae924282c4a70e35868b
Ronald Antonio Oliveira de sousa <ronaldkurouzo@gmail.com>
2024-10-16 13:21:14 -0300

    criando butoes  e filtro

---
+++ projeto-galeria/src/cityButton.js
@@ -0,0 +1,38 @@
+import $ from "jquery"
+
+const button = 600
+
+function filterByCity(city){
+    $("[wm-city]").each(function(i, e){
+        const isTarget = $(this).attr("wm-city") === city || city === null
+        if(isTarget){
+            $(this).fadeIn(button)
+        }
+        else{
+            $(this).fadeOut(button)
+        }
+    })
+}
+
+const cityButtons = $("[wm-city-buttons]")
+const cities = new Set
+
+$("[wm-city]").each(function(i, e){
+    cities.add($(e).attr("[wm-city]"))
+})
+
+const btns = Array.from(cities).map(city => {
+    const btn = $("<button>").addClass("btn","btn-primary").html(city)
+    btn.trigger("click", e => filterByCity(city))
+    return btn
+})
+
+const btnAll = $("<button>").addClass("btn","btn-primary").html("Todas")
+
+btnAll.trigger("click", e => filterByCity(null))
+btns.push(btnAll)
+
+const btngroup = $("<div>").addClass(["btn-group"])
+
+btngroup.append(btns)
+cityButtons.append(btngroup)
