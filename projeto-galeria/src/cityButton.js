f8059073f7efad6d87fcbd024c9e5735941291c2
Ronald Antonio Oliveira de sousa <ronaldkurouzo@gmail.com>
2024-10-17 19:58:19 -0300

    fix: final project

--- projeto-galeria/src/cityButton.js
+++ projeto-galeria/src/cityButton.js
@@ -1,38 +1,44 @@
 import $ from "jquery"
 
-const button = 600
+$.fn.cityButtons = function () {
+    const button = 600
 
-function filterByCity(city){
-    $("[wm-city]").each(function(i, e){
-        const isTarget = $(this).attr("wm-city") === city || city === null
-        if(isTarget){
-            $(this).fadeIn(button)
-        }
-        else{
-            $(this).fadeOut(button)
-        }
+    function filterByCity(city) {
+        $("[wm-city]").each(function (i, e) {
+            const isTarget = $(this).attr("wm-city") === city || city === null
+            if (isTarget) {
+                $(this).fadeIn(button)
+            }
+            else {
+                $(this).fadeOut(button)
+            }
+        })
+    }
+
+    const cityButtons = $("[wm-city-buttons]")
+    const cities = new Set
+
+    $("[wm-city]").each(function (i, e) {
+        cities.add($(e).attr("[wm-city]"))
     })
+
+    const btns = Array.from(cities).map(city => {
+        const btn = $("<button>").addClass("btn", "btn-primary").html(city)
+        btn.trigger("click", e => filterByCity(city))
+        return btn
+    })
+
+    const btnAll = $("<button>").addClass("btn", "btn-primary").html("Todas")
+
+    btnAll.trigger("click", e => filterByCity(null))
+    btns.push(btnAll)
+
+    const btngroup = $("<div>").addClass(["btn-group"])
+
+    btngroup.append(btns)
+    $(this).html(btngroup)
+
+    return this
 }
 
-const cityButtons = $("[wm-city-buttons]")
-const cities = new Set
-
-$("[wm-city]").each(function(i, e){
-    cities.add($(e).attr("[wm-city]"))
-})
-
-const btns = Array.from(cities).map(city => {
-    const btn = $("<button>").addClass("btn","btn-primary").html(city)
-    btn.trigger("click", e => filterByCity(city))
-    return btn
-})
-
-const btnAll = $("<button>").addClass("btn","btn-primary").html("Todas")
-
-btnAll.trigger("click", e => filterByCity(null))
-btns.push(btnAll)
-
-const btngroup = $("<div>").addClass(["btn-group"])
-
-btngroup.append(btns)
-cityButtons.append(btngroup)
+$("[wm-city-buttons").cityButtons()
